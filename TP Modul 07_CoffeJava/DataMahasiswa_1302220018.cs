917915c7c8991870fe2b2807d9e4b00192872d25
AdrianIchiro <adrianfahren10@gmail.com>
2024-04-01 00:31:06 +0700

    Menambahkan kelas DataMahasiswa_1302220018

---
+++ TP Modul 07_CoffeJava/DataMahasiswa_1302220018.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TP_Modul_07_CoffeJava
+{
+    internal class DataMahasiswa_1302220018
+    {
+        public NamaLengkap nama { get; set; }
+        public string nim { get; set; }
+        public string fakultas { get; set; }
+
+        public DataMahasiswa_1302220018()
+        {
+            nama = new NamaLengkap();
+        }
+        public class NamaLengkap
+        {
+
+            public string Depan;
+            public string Belakang;
+            public override string ToString()
+            {
+                return $"{Depan} {Belakang}";
+            }
+
+        }
+
+
+
+        public void ReadJSON()
+        {
+            string jsonContent = File.ReadAllText("D:\\Tugas\\Tugas Kuliah\\Semester 4\\KPL\\kelompok\\KPL_CoffeJava\\TP Modul 07_CoffeJava\\tp7_1_1302220018.json");
+            DataMahasiswa_1302220018 data = JsonConvert.DeserializeObject<DataMahasiswa_1302220018>(jsonContent);
+            this.nama = data.nama;
+            this.nim = data.nim;
+            this.fakultas = data.fakultas;
+        }
+
+    }
+
+
+
+
+}
--- TP Modul 07_CoffeJava/Program.cs
+++ TP Modul 07_CoffeJava/Program.cs
@@ -10,6 +10,13 @@ namespace TP_Modul_07_CoffeJava
     {
         static void Main(string[] args)
         {
+            DataMahasiswa_1302220018 mahasiswa = new DataMahasiswa_1302220018();
+            mahasiswa.ReadJSON();
+
+            Console.WriteLine("Nama: " + mahasiswa.nama);
+
+            Console.WriteLine("NIM: " + mahasiswa.nim);
+            Console.WriteLine("Fakultas: " + mahasiswa.fakultas);
         }
     }
 }
