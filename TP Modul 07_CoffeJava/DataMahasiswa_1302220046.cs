3c4bef54b4f6c24e970467296a6484c877f87b13
KoifishVEVO <149301244+KoifishVEVO@users.noreply.github.com>
2024-04-01 00:19:37 +0700

    menambahkan class DataMahasiswa_1302220046

---
+++ TP Modul 07_CoffeJava/DataMahasiswa_1302220046.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TP_Modul_07_CoffeJava
+{
+    public class DataMahasiswa_1302220046
+    {
+        public string NamaDepan { get; set; }
+        public string NamaBelakang { get; set; }
+        public int Nim { get; set; }
+        public string Fakultas { get; set; }
+
+        public void ReadJSON()
+        {
+            string jsonData = File.ReadAllText("C:\\Users\\LENOVO\\Downloads\\skool tugas\\Semester 4\\Konstruksi\\tp\\gruppen\\KPL_CoffeJava\\TP Modul 07_CoffeJava\\tp7_1_1302220046.json");
+
+            DataMahasiswa_1302220046 data = JsonConvert.DeserializeObject<DataMahasiswa_1302220046>(jsonData);
+
+            Console.WriteLine($"Nama {data.NamaDepan} {data.NamaBelakang} dengan nim {data.Nim} dari fakultas {data.Fakultas}");
+        }
+    }
+}
--- TP Modul 07_CoffeJava/Program.cs
+++ TP Modul 07_CoffeJava/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace TP_Modul_07_CoffeJava
 {
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            //NONE IMplementation
+
+            DataMahasiswa_1302220046 mahasiswa = new DataMahasiswa_1302220046();
+
+
+            mahasiswa.ReadJSON();
+
+            Console.WriteLine("Nama Depan: " + mahasiswa.NamaDepan);
+            Console.WriteLine("Nama Belakang: " + mahasiswa.NamaBelakang);
+            Console.WriteLine("NIM: " + mahasiswa.Nim);
+            Console.WriteLine("Fakultas: " + mahasiswa.Fakultas);
         }
     }
 }
