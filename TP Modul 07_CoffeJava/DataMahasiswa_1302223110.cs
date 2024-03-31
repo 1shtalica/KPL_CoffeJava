c150c136aea0a868b54f4335f3cec27e6c43ca74
1shtalica <adibfaizulhaq05@gmail.com>
2024-03-31 23:06:10 +0700

    menambahkan class DataMahasiswa1302223110

---
+++ TP Modul 07_CoffeJava/DataMahasiswa_1302223110.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+using Newtonsoft.Json;
+
+namespace TP_Modul_07_CoffeJava
+{
+    internal class DataMahasiswa_1302223110
+    {
+        //nim: 1302223110
+
+        public namaMahasiswa nama{ get; set; }
+        public string Nim { get; set; }
+        public string Fakultas { get; set; }
+
+        public DataMahasiswa_1302223110()
+        {
+            nama = new namaMahasiswa();
+        }
+
+        public class namaMahasiswa
+        {
+            public string depan;
+            public string belakang;
+            public override string ToString()
+            {
+                return $"{depan}  {belakang}";
+            }
+        }
+        
+        public void ReadJSON()
+        {
+            string jsonFilePath = File.ReadAllText(@"D:\\Kuliah\\Tugas\\MATKUL\\Semester 4\\Konstruksi Perangkat Lunak\\TUBES\\TP Modul 07_CoffeJava\\tp7_1_1302223110.json");
+            DataMahasiswa_1302223110 mahasiswa = JsonConvert.DeserializeObject<DataMahasiswa_1302223110>(jsonFilePath);
+
+            this.nama= mahasiswa.nama;
+            this.Nim = mahasiswa.Nim;
+            this.Fakultas = mahasiswa.Fakultas;
+               
+        }
+    }
+}
--- TP Modul 07_CoffeJava/Program.cs
+++ TP Modul 07_CoffeJava/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TP_Modul_07_CoffeJava
 {
@@ -10,6 +11,12 @@ namespace TP_Modul_07_CoffeJava
     {
         static void Main(string[] args)
         {
+            DataMahasiswa_1302223110 adib = new DataMahasiswa_1302223110();
+            adib.ReadJSON();
+
+            Console.WriteLine("Nama : " + adib.nama);
+            Console.WriteLine("nim: " + adib.Nim);
+            Console.WriteLine("Fakultas: " + adib.Fakultas);
         }
     }
 }
